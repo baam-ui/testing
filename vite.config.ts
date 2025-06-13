86f710e5fd0a354e72d733dc52279dc050fb5da0
baam-ui <baamcryp@gmail.com>
2025-06-13 12:25:03 +0800

    Fix deployment issues

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,22 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'esbuild',
+    target: 'es2015',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          icons: ['lucide-react']
+        }
+      }
+    }
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  base: './'
 });
